2b9be9dc9d04526315ef1e2ed2021f59d07fc755
awosborn <alanturing@example.coclear>
2020-11-11 09:24:17 -0700

    Add check_stock test

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -7,6 +7,8 @@ class VendorTest < Minitest::Test
   
   def setup
     @vendor = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
   end
 
   def test_it_exsist_and_has_attributes
@@ -15,5 +17,10 @@ class VendorTest < Minitest::Test
     assert_instance_of Vendor, @vendor
     assert_equal "Rocky Mountain Fresh", @vendor.name
     assert_equal expected, @vendor.inventory
+  end
+
+  def test_check_stock
+
+    assert_equal 0, @vendor.check_stock(@item1)
   end
 end
