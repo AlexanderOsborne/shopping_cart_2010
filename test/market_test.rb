fe2aa92b99572e483bc65964c8298abc32b5ee39
awosborn <alanturing@example.coclear>
2020-11-11 10:27:15 -0700

    add test_total_inventory

--- test/market_test.rb
+++ test/market_test.rb
@@ -45,6 +45,7 @@ class MarketTest < Minitest::Test
   end
 
   def test_vendors_that_sell
+
     @market.add_vendor(@vendor1)
     @market.add_vendor(@vendor2)
     @market.add_vendor(@vendor3)
@@ -56,5 +57,21 @@ class MarketTest < Minitest::Test
 
     assert_equal [@vendor1, @vendor3], @market.vendors_that_sell(@item1)
     assert_equal [@vendor2], @market.vendors_that_sell(@item4)
+  end
+
+  def test_total_inventory
+
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+
+    expected = 21
+    assert_equal expected, @market.total_inventory
   end
 end
