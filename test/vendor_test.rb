780198ce1c2fdd9be94be1cea4f6b39148c3dd62
awosborn <alanturing@example.coclear>
2020-11-11 09:19:49 -0700

    Add vendor test

---
+++ test/vendor_test.rb
@@ -0,0 +1,18 @@
+require 'Minitest/autorun'
+require 'Minitest/pride'
+require './lib/vendor'
+require './lib/item'
+
+class VendorTest < Minitest::Test
+  
+  def setup
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  def test_it_exsist_and_has_attributes
+
+    assert_instance_of Vendor, @vendor
+    assert_equal "Rocky Mountain Fresh", @vendor.name
+    assert_equal "{}", @vendor.inventory
+  end
+end
