d0aba218057dc14e5999c2f9fd63b2747098f843
awosborn <alanturing@example.coclear>
2020-11-11 09:45:27 -0700

    Add test_add_vendor and add_vendor

--- test/market_test.rb
+++ test/market_test.rb
@@ -8,11 +8,30 @@ class MarketTest < Minitest::Test
   
   def setup
     @market = Market.new("South Pearl Street Farmers Market")
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor3 = Vendor.new("Palisade Peach Shack")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
   end
 
   def test_it_exsist_and_has_attributes
 
     assert_instance_of Market, @market
     assert_equal "South Pearl Street Farmers Market", @market.name
+    assert_equal [], @market.vendors
+  end
+
+  def test_add_vendor
+
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    expected = [@vendor1,@vendor2,@vendor3]
+
+    assert_equal expected, @market.vendors
   end
 end
